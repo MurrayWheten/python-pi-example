0ff98e6888d53b8bc84efc78d0957b01abeec949
MurrayWheten <mdwheten@gmail.com>
2020-01-16 21:38:30 -0400

    New Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-pi-example
 This is an example to demonstate how GIT works with an IDE (PyCharm) and POP OS
+
+Make a change
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def main(msg):
+    // Another Comment
     // Adding a different comment
     // Print a message
     print(msg)
